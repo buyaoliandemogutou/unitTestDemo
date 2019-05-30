e757fe3858713c0dbd34b62078d0453ce3328272
buyaoliandemogutou <1096902145@qq.com>
2019-05-30 20:00:18 +0800

    Initial commit

---
+++ venv/unitTest.py
@@ -0,0 +1,35 @@
+import unittest
+import HTMLTestReportEN
+
+class MyTest(unittest.TestCase):
+    def terDown(self):
+        print('每个测试用例执行之后做操作')
+
+    def setUp(self):
+        print('2每个测试用例执行之前做操作')
+
+    @classmethod
+    def tearDownClass(self):
+        print('必须使用 @ classmethod装饰器, 所有test运行完后运行一次')
+    @classmethod
+    def setUpClass(self):
+        print('必须使用 @ classmethod装饰器, 所有test运行完钱运行一次')
+
+    def test_first_run(self):
+        print('test_a_run')
+        self.assertEqual(1,1)
+
+    def test_b_run(self):
+        print('1test_b_run')
+        self.assertEqual(2,2)
+
+
+if __name__ == '__main__':
+    test_suite=unittest.TestSuite()
+    test_suite.addTest(MyTest('test_first_run'))
+    test_suite.addTest(MyTest('test_b_run'))
+    fp=open('res.html','wb')
+    runner=HTMLTestReportEN.HTMLTestRunner(stream=fp,title='api测试报告',tester='ZhaoJun')
+    runner.run(test_suite)
+
+
